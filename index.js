88b3ccdf3cfbeaf7de1c71a56735cd5f8ecb9155
Or Bachar <or@localcoin.com>
2019-07-23 16:28:19 +0300

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    globals: {
+        MyGlobal: true
+    },
+
+    rules: {
+        "comma-spacing": [2, { "before": false, "after": true }],
+        "no-eval": 2,
+        "no-multiple-empty-lines": [2, { "max": 2 }],
+        "quotes": [2, "single"],
+        "yoda": 2
+    }
+
+};
