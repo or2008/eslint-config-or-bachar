349a9e02b94fee424ed1f086ecab1db08adeb9b5
Or Bachar <or@localcoin.com>
2019-08-16 16:30:40 +0300

    update rules

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ module.exports = {
         "no-eval": 2,
         "no-multiple-empty-lines": [2, { "max": 2 }],
         "quotes": [2, "single"],
-        "yoda": 2
+        "yoda": 2,
+        "no-console": 1,
+        "semi": 2,
+        "no-var": 2,
+        "no-extra-parens": 2,
+        "curly": [2, "multi-or-nest", "consistent"],
+        "no-empty-function": [2, {"allow": [ "arrowFunctions"]}],
+        "func-style": [2,"declaration", { "allowArrowFunctions": true }],
+        "arrow-body-style": [0, "as-needed"],
+        "arrow-parens": [2, "as-needed"],
+        "no-unused-vars": 2,
+        "no-use-before-define": [2, { "functions": false }],
+        "@typescript-eslint/no-use-before-define": [2, { "functions": false }]
     }
 
 };
