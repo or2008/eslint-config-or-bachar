5d3f34b50508448265fb8aa60562d914fd1cea2d
Or Bachar <or2008@gmail.com>
2020-06-12 16:46:12 +0300

    updates

--- index.js
+++ index.js
@@ -10,18 +10,16 @@ module.exports = {
         "quotes": [2, "single"],
         "yoda": 2,
         "no-console": 1,
-        "semi": 2,
         "no-var": 2,
         "no-extra-parens": 2,
         "curly": [2, "multi-or-nest", "consistent"],
-        "no-empty-function": [2, {"allow": [ "arrowFunctions"]}],
-        "func-style": [2,"declaration", { "allowArrowFunctions": true }],
+        "no-empty-function": [2, { "allow": ["arrowFunctions"] }],
+        "func-style": [2, "declaration", { "allowArrowFunctions": true }],
         "arrow-body-style": [0, "as-needed"],
         "arrow-parens": [2, "as-needed"],
         "no-unused-vars": 2,
         "no-use-before-define": [2, { "functions": false, "classes": false }],
-        "@typescript-eslint/no-use-before-define": [2, { "functions": false, "classes": false }]
-         "semi": "error",
+        "@typescript-eslint/no-use-before-define": [2, { "functions": false, "classes": false }],
         'no-console': ['warn', { allow: ['warn', 'error'] }],
         'eqeqeq': ['warn', 'always'],
         'prefer-const': ['error', { destructuring: 'all', ignoreReadBeforeAssign: true }],
@@ -33,9 +31,6 @@ module.exports = {
         '@typescript-eslint/ban-ts-ignore': 1,
         '@typescript-eslint/no-this-alias': 0,
         '@typescript-eslint/camelcase': 1,
-        '@typescript-eslint/triple-slash-reference': [
-          'error',
-          { path: 'always', types: 'never', lib: 'never' },
-        ]
+        '@typescript-eslint/triple-slash-reference': ['error', { path: 'always', types: 'never', lib: 'never' }]
     }
 };
