3dd606c8b38c788a0686218bd6b45938d258228a
Or Bachar <or2008@gmail.com>
2020-06-12 16:44:36 +0300

    new rules

--- index.js
+++ index.js
@@ -21,5 +21,21 @@ module.exports = {
         "no-unused-vars": 2,
         "no-use-before-define": [2, { "functions": false, "classes": false }],
         "@typescript-eslint/no-use-before-define": [2, { "functions": false, "classes": false }]
+         "semi": "error",
+        'no-console': ['warn', { allow: ['warn', 'error'] }],
+        'eqeqeq': ['warn', 'always'],
+        'prefer-const': ['error', { destructuring: 'all', ignoreReadBeforeAssign: true }],
+        '@typescript-eslint/indent': ['error', 4, { VariableDeclarator: 4, SwitchCase: 1 }],
+        '@typescript-eslint/no-unused-vars': 0,
+        '@typescript-eslint/interface-name-prefix': 0,
+        '@typescript-eslint/explicit-member-accessibility': 0,
+        '@typescript-eslint/no-triple-slash-reference': 0,
+        '@typescript-eslint/ban-ts-ignore': 1,
+        '@typescript-eslint/no-this-alias': 0,
+        '@typescript-eslint/camelcase': 1,
+        '@typescript-eslint/triple-slash-reference': [
+          'error',
+          { path: 'always', types: 'never', lib: 'never' },
+        ]
     }
 };
